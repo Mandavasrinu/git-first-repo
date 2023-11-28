29234fa53183657edcdeaca468a84668c3920e5b
Mandavasrinu <srinumandavaaws@outlook.com>
2023-11-28 14:32:59 +0530

    Version 1 files committed

---
+++ Booking.java
@@ -0,0 +1,14 @@
+1. git init --> create empty repo --> to perform git operations (linux based commands)
+2. git status --> verify files / programs eighter tracked files / untracked files
+3. a) git add filename (single) --> if you have untracked files those file make as tracked files
+ b) git add . (multiple files / all files) --> if yu have untracked files those file make as tracked files.
+
+4. git reset filename --> tracked file to untracked file.
+
+5. git commit --> commit all the files make it as final for public/private access
+
+6. git log --> this command is telling to you how many commits you done
+
+7. git remote add origin URL --> connection between Local git repo and git hub repo
+
+8. git push -u origion master --> your local code adding to remote repo
